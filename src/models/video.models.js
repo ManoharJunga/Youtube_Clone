03d0f12831b1dbe367decc98179e2f4d58c1affe
ManoharJunga <95352064+ManoharJunga@users.noreply.github.com>
2024-05-30 08:07:37 +0530

    video model

--- src/models/video.models.js
+++ src/models/video.models.js
@@ -1,6 +1,39 @@
 import mongoose, {Schema} from "mongoose";
 
 const videoSchema = Schema.create({
+    videoFile: {
+        type: String, //cloudinary url
+        required: true
+    },
+    thumbnail: {
+        type: String, //cloudinary url
+        required: true
+    },
+    title: {
+        type: String, 
+        required: true
+    },
+    description: {
+        type: String, 
+        required: true
+    },
+    duration: {
+        type: Number,
+        required: true
+    },
+    views: {
+        type: Number,
+        default: 0
+    },
+    isPublished: {
+        type: Boolean,
+        default: true,
+    },
+    owner: {
+        type: Schema.Types.ObjectId,
+        ref: 'User',
+        required: true
+    }
 
 },{timestamp: true})
 
