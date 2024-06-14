625bc75e2878a67c4eabcecd3c5867542b33867f
ManoharJunga <95352064+ManoharJunga@users.noreply.github.com>
2024-06-14 16:20:30 +0530

    commit

--- src/app.js
+++ src/app.js
@@ -1,17 +1,21 @@
 import express from 'express';
-import cors from "cors";
+import cors from 'cors';
 import cookieParser from 'cookie-parser';
+import userRouter from './routes/user.routes.js'; // Ensure the extension is correct
 
 const app = express();
 
 app.use(cors({
-    origin: process.env.CORS_ORGIN,
+    origin: process.env.CORS_ORIGIN, 
     credentials: true
 }));
 
-app.use(express.json({limit: "16kb"}))
-app.use(express.urlencoded({extended: true, limit: "16kb"}))
-app.use(express.static("public"))
-app.use(cookieParser())
+app.use(express.json({ limit: '16kb' }));
+app.use(express.urlencoded({ extended: true, limit: '16kb' }));
+app.use(express.static('public'));
+app.use(cookieParser());
 
-export { app }
+// Routes declaration
+app.use('/api/v1/users', userRouter);
+
+export { app };
